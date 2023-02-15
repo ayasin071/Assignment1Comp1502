24819e36478329e03f5febdd0a44371471932e5a
osmanmufti <113641656+osmanmufti@users.noreply.github.com>
2023-02-14 23:20:38 -0700

    work
    dfg

---
+++ Lab 4 - Skeleton/src/mru/lab4/application/GeometryDemo.java
@@ -0,0 +1,16 @@
+package mru.lab4.application;
+
+import mru.lab4.project.Geometry;
+
+public class GeometryDemo {
+
+	public static void main(String[] args) {
+		//Creating an instance of the Geometry class
+		Geometry geo = new Geometry();
+		
+		//Calling the runApplication method
+		geo.runApplication();
+
+	}
+
+}
